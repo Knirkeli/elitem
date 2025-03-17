80a65cb0df3064b0d69cc92b86d7d0337f0fd38b
Eirik Kirkeli <115043925+Knirkeli@users.noreply.github.com>
2025-03-17 15:33:00 +0100

    component annotation

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,33 @@
+// /** @type {import('next').NextConfig} */
+
+// const nextConfig = {
+//   async redirects() {
+//     return [
+//       {
+//         source: '/index',
+//         destination: '/',
+//         permanent: true,
+//       },
+//       {
+//         source: '/_not-found',
+//         destination: '/404',
+//         permanent: false,
+//       },
+//     ];
+//   },
+//   images: {
+//     unoptimized: true,
+//     remotePatterns: [
+//       {
+//         protocol: "https",
+//         hostname: "cdn.sanity.io",
+//       },
+//     ],
+//   },
+// };
+
+// export default nextConfig;
+
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
@@ -23,6 +53,9 @@ const nextConfig = {
         hostname: "cdn.sanity.io",
       },
     ],
+  },
+  reactComponentAnnotation: {
+    enabled: process.env.NODE_ENV === 'development',
   },
 };
 
