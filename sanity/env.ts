073ddcd8f2eebbe2e6c15ece7f34fd160e857520
Eirik Kirkeli <115043925+Knirkeli@users.noreply.github.com>
2025-03-17 12:56:01 +0100

    prøver med dummy verdier for resend

--- sanity/env.ts
+++ sanity/env.ts
@@ -1,3 +1,26 @@
+// export const apiVersion =
+//   process.env.NEXT_PUBLIC_SANITY_API_VERSION || "2024-10-31";
+
+// export const dataset = assertValue(
+//   process.env.NEXT_PUBLIC_SANITY_DATASET,
+//   "Missing environment variable: NEXT_PUBLIC_SANITY_DATASET"
+// );
+
+// export const projectId = assertValue(
+//   process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+//   "Missing environment variable: NEXT_PUBLIC_SANITY_PROJECT_ID"
+// );
+
+// export const useCdn = false;
+
+// function assertValue<T>(v: T | undefined, errorMessage: string): T {
+//   if (v === undefined) {
+//     throw new Error(errorMessage);
+//   }
+
+//   return v;
+// }
+
 export const apiVersion =
   process.env.NEXT_PUBLIC_SANITY_API_VERSION || "2024-10-31";
 
@@ -12,6 +35,9 @@ export const projectId = assertValue(
 );
 
 export const useCdn = false;
+
+export const resendApiKey = process.env.RESEND_API_KEY || "dummy_value";
+export const resendAudienceId = process.env.RESEND_AUDIENCE_ID || "dummy_value";
 
 function assertValue<T>(v: T | undefined, errorMessage: string): T {
   if (v === undefined) {
